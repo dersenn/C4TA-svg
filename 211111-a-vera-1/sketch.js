a566a9d3a262c58ae025234f4776be7673a43e74
dersenn <67318888+dersenn@users.noreply.github.com>
2021-11-14 00:07:16 +0100

    Split files. vera2 creaeted.

--- 211111-a-vera-1/sketch.js
+++ 211111-a-vera-1/sketch.js
@@ -19,53 +19,9 @@ function drawCenterGrid() {
 
 let colors = ["#ff0000", "#00ff00", "#0000ff"]
 
-
-
-// VERA 1
-
-// needs work on the "border". should stay the same... no matter how many divisions.
-// maybe has to do with that ngn.scale thing...
-
-function vera1(xPos, yPos, w, h, divisions) {
-  let cols = divisions
-  let rows = cols
-
-  let o = 3 // meaning tiles overlap by a third
-
-  // those have been calculated by some solving of problems in calculations.js.
-  let tileW = ( w * o ) / ( -cols + 3 + cols * o ) 
-  let tileH = ( h * o ) / ( -rows + 3 + rows * o )
-
-  let xOverlap = tileW / o
-  let yOverlap = tileH / o
-
-  xPos += xOverlap
-  yPos += yOverlap
-
-  for (let x = 0; x < cols; x++) {
-    let xOff = xPos + x * (tileW - xOverlap)
-    for (let y = 0; y < rows; y++) {
-      let yOff = yPos + y * (tileH - yOverlap)
-
-      drawVeraElement(xOff, yOff, tileW, tileH)
-    }
-  }
-
-  function drawVeraElement(xPos, yPos, w, h) {
-    let positions = [
-      [{x: xPos, y: yPos},{x: xPos + w, y: yPos + h}],
-      [{x: xPos, y: yPos + h},{x: xPos + w, y: yPos}],
-      [{x: xPos + w/2, y: yPos},{x: xPos + w/2, y: yPos + h}],
-      [{x: xPos, y: yPos + h/2},{x: xPos + w, y: yPos + h/2}]
-     ]
-
-    ngn.makeSvgLine({ parent: dom.svgLayer, id: "vera1_", stroke: .2, d: ngn.svgPath(positions[getRandomInt(0, positions.length)]), color: colors[getRandomInt(0, colors.length)] })
-  }
-}
-
 // DRAW STUFF
 
-drawCenterGrid()
+// drawCenterGrid()
 
 vera1(-ngn.min/2, -ngn.min/2, ngn.min, ngn.min, 12)
 
