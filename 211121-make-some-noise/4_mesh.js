06417a74225e38e98420fd740665ae61444fd12a
dersenn <67318888+dersenn@users.noreply.github.com>
2021-11-22 16:11:23 +0100

    mesh! now get it to respect bounds ;-)

--- 211121-make-some-noise/4_mesh.js
+++ 211121-make-some-noise/4_mesh.js
@@ -1,20 +1,119 @@
-// SETUP STUFF
+// FUNCTIONS
 
+mesh = {}
 
-//drehung
-lineRotation = function ({ point, long, rotation }) {
-    return [
-        { x: point.x, y: point.y },
-        { x: point.x + Math.sin(rotation) * long, y: point.y + Math.cos(rotation) * long }
-    ];
+mesh.path = function (ia, close = false) {
+    let output = "M ";
+    for (var i = 0; i < ia.length; i++) {
+        output += ia[i].pos.x * ngn.res + " " + ia[i].pos.y * ngn.res + " ";
+    }
+    if (close) {
+      output += "z";
+    }
+    return output;
 };
+
+mesh.paths = function (ia, close = false) {
+  let output = "";
+  for (var i = 0; i < ia.length; i++) {
+      output += mesh.path(ia[i], close);
+  }
+  return output;
+};
+
+
+mesh.dot = function(io) {
+  let output = "M " + io.pos.x * ngn.res + " " + io.pos.y * ngn.res + " z"
+  return output
+}
+
+mesh.dots = function(ia) {
+  let output = ""
+  for(i = 0; i < ia.length; i++) {
+    output += mesh.dot(ia[i])
+  }
+  return output
+}
+
+mesh.connections = function(ia) {
+  let output
+}
+
+// SETUP
 
 let simplex = new SimplexNoise();
 
 
-// SETUP
-
 let nCols = 10
 let nRows = nCols
 
+let cell = {
+  w: ngn.width / nCols,
+  h: ngn.height / nRows
+}
+
 // MAKE MESH WITH NOISE
+
+// create initial points on grid
+let points = []
+for (let x = 0; x < nCols; x++) {
+  let cols = []
+  for (let y = 0; y < nRows; y++) {
+    let xOff = (-ngn.width/2 + x * cell.w) + cell.w / 2
+    let yOff = (-ngn.height/2 + y * cell.h) + cell.h / 2
+    let pos = {x: xOff, y: yOff}
+    let i = {x: x, y: y}
+    cols.push({i, pos})
+  }
+  points.push(cols)
+}
+// console.log(points)
+
+let connections = []
+// assign connections to each point
+for (let x = 0; x < nCols - 1; x++) {
+  for (let y = 0; y < nRows - 1; y++) {
+    let pa = points[x][y]
+    let pb = points[x][y + 1]
+    let pc = points[x + 1][y + 1]
+    let pd = points[x + 1][y]
+    connections.push([pa, pb, pc, pd])
+  }
+}
+console.log(connections)
+
+// let path = mesh.paths(connections, true)
+
+svg.makeLine({
+  parent: dom.svgLayer,
+  id: "cells",
+  color: "#0f0",
+  cap: "round",
+  stroke: .3
+})
+
+
+
+// ANIMATE
+let res = 0.0003
+
+function loop(t) {
+
+  for (let i = 0; i < connections.length; i++) {
+    let perlin = simplex.noise2D (t * res, i)
+    for (let j = 0; j < connections[i].length; j++) {
+      p = connections[i][j]
+      // console.log(p)
+      p.pos.x += perlin * cell.w/1000
+      p.pos.y += perlin * cell.w/1000
+    }
+  }
+
+  let path = mesh.paths(connections, true)
+
+  dom.cells.setAttributeNS(null, "d", path)
+
+  requestAnimationFrame(loop)
+}
+
+loop(0)
