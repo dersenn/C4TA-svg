905f4bca33f67adbec7f67e7d0ebcb00824dff2c
dersenn <67318888+dersenn@users.noreply.github.com>
2021-11-22 01:13:08 +0100

    two loops :-)

--- 211121-make-some-noise/sketch.js
+++ 211121-make-some-noise/sketch.js
@@ -22,6 +22,13 @@ let step = {x: ngn.width/nCols, y: ngn.height/nRows}
 let position = { x: -ngn.width/2 + step.x/2, y: -ngn.height/2 + step.y/2}
 
 
+
+let resolution = .05
+let speed = .0003
+
+
+// green
+
 svg.makeLine({
   parent: dom.svgLayer, 
   id: "wave",
@@ -33,17 +40,7 @@ svg.makeLine({
 });
 
 
-
-let resolution = .05
-let speed = .0003
-
-let i = 0
-
-
 let loop1 = function loop(time) {
-
-  // let test = simplex.noise2D(i , resolution * time)
-  // console.log(Math.floor(mapValues(test, -1, 1, 0, 100)))
 
   let lines = []
   for (let y = 0; y < nRows; y++) {
@@ -65,7 +62,49 @@ let loop1 = function loop(time) {
   dom["wave"].setAttributeNS(null, "stroke", rgbToHex(0,col,0))
 
   requestAnimationFrame(loop)
-  // i += 1
 }
 
 loop1(0)
+
+
+// red
+
+svg.makeLine({
+  parent: dom.svgLayer, 
+  id: "wave2",
+  cap: "round", 
+  stroke: 1, 
+  color: "#000", 
+  d: ""
+  // d: svg.path(points, false)
+});
+
+
+const loop2 = function loop(time) {
+
+  let lines = []
+  for (let y = 0; y < nRows; y++) {
+    let points = []
+    // let posY = (position.y + y * step.y) + mapValues(simplex.noise2D(y * resolution, 1), -1, 1, -step.y/2, step.y/2)
+    let posY = position.y + y * step.y
+    let yOff = position.y + y * step.y
+    for (let x = 0; x < nCols; x++) {
+      let ampY = simplex.noise3D(x * resolution, y * resolution, time * speed)
+      yOff = /*posY*/ posY + (mapValues(ampY, -1, 1, -step.y/2, step.y/2))
+      let xOff = position.x + x * step.x
+      points.push({x: xOff, y: yOff})
+    }
+    lines.push(points)
+  }
+
+  let col = simplex.noise2D(time/500, 0)
+  col = mapValues(col, -1, 1, 0, 255)
+
+  dom["wave2"].setAttributeNS(null, "d", svg.paths(lines));
+  dom["wave2"].setAttributeNS(null, "stroke", rgbToHex(col,0,0))
+
+  requestAnimationFrame(loop)
+}
+
+loop2(0)
+
