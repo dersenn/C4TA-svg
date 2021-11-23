c0338ba5cfc64984ca7d9a8a57a9742b4f3a3be3
dersenn <67318888+dersenn@users.noreply.github.com>
2021-11-24 00:02:31 +0100

    bezier-1 proof-of-concept. thx, chrett.

--- 211123-bezier/bezier-1.js
+++ 211123-bezier/bezier-1.js
@@ -1,27 +1,65 @@
 // FUNCTIONS
 
+svg.lineSoft = function (ia) {
 
+    //clone first and last point
+    ia.unshift(ia[0]);
+    ia.push(ia[ia.length - 1]);
 
-// SETUP STUFF
+    let output = "M ";
+    for (let i = 1; i < ia.length; i++) {
+        output += (ia[i - 1].x + ia[i].x) / 2 * ngn.res + "," + (ia[i - 1].y + ia[i].y) / 2 * ngn.res + " Q";
+        output += ia[i].x * ngn.res + "," +ia[i].y * ngn.res + " ";
+    }
+
+    //end point
+    output += ia[ia.length - 1].x * ngn.res + "," + ia[ia.length - 1].y * ngn.res + " ";
+    return output;
+};
+
 
 // SETUP
-let simplex = new SimplexNoise();
+// let simplex = new SimplexNoise();
 
+let nPoints = 5
 
-// MAKE MESH WITH NOISE
+let points = []
 
-// create initial points on grid
+for (let p = 0; p < nPoints; p++) {
+  points.push({x: mapValues(Math.random(), 0, 1, -ngn.width/2, ngn.width/2), y: mapValues(Math.random(), 0, 1, -ngn.height/2, ngn.height/2)})
+}
 
+let path = svg.lineSoft(points)
+
+let cpts = svg.dots(points)
+
+// DRAW
+
+svg.makeLine({
+  parent: dom.svgLayer,
+  id: "bezier",
+  color: "#0f0",
+  cap: "round",
+  d: path,
+  stroke: .2
+})
+
+svg.makeLine({
+  parent: dom.svgLayer,
+  id: "cpts",
+  color: "#f00",
+  cap: "round",
+  d: cpts,
+  stroke: 1
+})
 
 // ANIMATE
 
-function draw(t) {
+// function draw(t) {
 
+//   dom["bezier"].setAttributeNS(null, "d", path)
 
-  requestAnimationFrame(draw)
-}
+//   requestAnimationFrame(draw)
+// }
 
-
-draw(0)
-
-
+// draw(0)
