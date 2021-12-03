97d7fcf911c5c45645b01b12bcd28ad45045ebac
dersenn <67318888+dersenn@users.noreply.github.com>
2021-12-03 21:43:25 +0100

    added unfinished bezier function to engine...

--- 211203-machine/engine.js
+++ 211203-machine/engine.js
@@ -122,6 +122,64 @@ svg.makeShape = function ({ parent, id, d = "", color = "#ff00ff" }) {
     parent.appendChild(dom[id]);
 };
 
+svg.bezierPath = function(pts, t, closed) {
+  let cp = []
+  let n = pts.length
+  let output = ""
+
+  // Append and prepend first and last point to generate control points
+  pts.push(pts[0])
+  pts.unshift(pts[n-1])
+  for (let i = 0; i < n; i++) {
+    cp.push(getControlPoints(pts[i], pts[i+1], pts[i+2], t))
+  }
+  // to make the loop afterwards a bit easier do the same for generated cps.
+  cp.push(cp[0])
+  cp.unshift(cp[n-1])
+
+  output += "M " + pts[1].x * ngn.res + "," + pts[1].y * ngn.res + " "
+
+  if (closed) {
+    for (let i = 1; i < n+1; i++) {
+      let cp1 = cp[i][1]
+      let cp2 = cp[i+1][0]
+      let ep = pts[i+1]
+
+      output += "C " + 
+        cp1.x * ngn.res + "," + cp1.y * ngn.res + " " + 
+        cp2.x * ngn.res + "," + cp2.y * ngn.res + " " + 
+        ep.x * ngn.res + "," + ep.y * ngn.res
+    }
+  } else {
+    // change to quadratic bezier for first and last point. see example.js
+    // so, not quite there yet
+    for (let i = 1; i < n; i++) {
+      let cp1 = cp[i][1]
+      let cp2 = cp[i+1][0]
+      let ep = pts[i+1]
+
+      output += "C " + 
+        cp1.x * ngn.res + "," + cp1.y * ngn.res + " " + 
+        cp2.x * ngn.res + "," + cp2.y * ngn.res + " " + 
+        ep.x * ngn.res + "," + ep.y * ngn.res
+    }
+  }
+  output += " z"
+  return output
+}
+
+// adapted from this: http://scaledinnovation.com/analytics/splines/aboutSplines.html
+function getControlPoints(p0, p1, p2, t) {
+  let d01 = Math.sqrt(Math.pow(p1.x - p0.x, 2) + Math.pow(p1.y - p0.y, 2)); // distance between pt1 and pt2
+  let d12 = Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2)); // distance between pt2 and pt3
+  let fa = t * d01 / (d01+d12);   // scaling factor for triangle Ta
+  let fb = t * d12 / (d01+d12);   // ditto for Tb, simplifies to fb=t-fa
+  let cp1x = p1.x - fa * (p2.x - p0.x);    // x2-x0 is the width of triangle T
+  let cp1y = p1.y - fa * (p2.y - p0.y);    // y2-y0 is the height of T
+  let cp2x = p1.x + fb * (p2.x - p0.x);
+  let cp2y = p1.y + fb * (p2.y - p0.y);  
+  return [{x: cp1x, y: cp1y}, {x: cp2x, y: cp2y}];
+}
 
 // DOM
 
